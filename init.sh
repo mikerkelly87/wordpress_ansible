12b2c31368741792c009da4807c46cadea579bc0
Mike Kelly <mikerkelly87@gmail.com>
2019-07-04 04:20:25 -0500

    Fixing playbook name

--- init.sh
+++ init.sh
@@ -9,4 +9,4 @@ sudo DEBIAN_FRONTEND=noninteractive apt install -y ansible
 # Make a softlink to put the wordpress role in the default location Ansible expects roles to be in
 sudo ln -s /root/wordpress_ansible/playbooks/wp /etc/ansible/roles/
 # Run the playbook to apply the role
-sudo ansible-playbook /root/wordpress_ansible/playbooks/wp.yml
+sudo ansible-playbook /root/wordpress_ansible/playbooks/wordpress_ansible.yml
