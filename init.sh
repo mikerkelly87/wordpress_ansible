9a3b3f4844e57a654a7a020b3fdae02231398384
Mike Kelly <mikerkelly87@gmail.com>
2019-07-04 04:23:32 -0500

    fixing path

--- init.sh
+++ init.sh
@@ -7,6 +7,6 @@ sudo apt update
 # Install Ansible
 sudo DEBIAN_FRONTEND=noninteractive apt install -y ansible
 # Make a softlink to put the wordpress role in the default location Ansible expects roles to be in
-sudo ln -s /root/wordpress_ansible/playbooks/wp /etc/ansible/roles/
+sudo ln -s /root/wordpress_ansible/wp /etc/ansible/roles/
 # Run the playbook to apply the role
 sudo ansible-playbook /root/wordpress_ansible/playbooks/wordpress_ansible.yml
